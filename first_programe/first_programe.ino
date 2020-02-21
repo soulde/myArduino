7fd4facbeff847178a7abcfb2ab0ef37b5fe162f
soulde <2780828251@qq.com>
2020-02-21 20:53:29 +0800

    explain

--- first_programe/first_programe.ino
+++ first_programe/first_programe.ino
@@ -1,29 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(12,OUTPUT);
+void setup()
+{
+  //set LEDs for feedback
+  pinMode(12, OUTPUT);
+  pinMode(7, OUTPUT);
+
+  //set Serialport
   Serial.begin(9600);
-
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int data=0;
-  
-  while(1)
+int data = 0;
+int i;
+void loop()
+{
+  while (1)
   {
-    int i=0;
-  if(Serial.available())
-  {
-    data=Serial.read();
-    for(;i<data;i++)
+    if (Serial.available())
     {
-      digitalWrite(12,HIGH);
-      delay(600);
-      digitalWrite(12,LOW);
-      delay(600);
+      //recieve the data for computer
+      data = Serial.read();
+      delay(100);
+      //data is the int angle in c++ code,to minimize the blink times
+      data %= 10;
+
+      //blink
+      for (i = 0; i < data; i++)
+      {
+        digitalWrite(12, HIGH);
+        delay(400);
+        digitalWrite(12, LOW);
+        delay(400);
+      }
+
+      //send a single as sending data back to computer
+      digitalWrite(7, HIGH);
+      delay(500);
+      digitalWrite(7, LOW);
+      delay(500);
+
+      Serial.write(i);
+
+      //wait for sending code finished
+      Serial.flush();
+
+      //clear the datas in flash
+      for (; Serial.read() >= 0;);
     }
-    Serial.write(i);
-    Serial.flush();
   }
-}
 }
